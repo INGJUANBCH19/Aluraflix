8237f9a9ff09a23f53bbda78e5609b172077f6bc
Juan Camilo Betin <ingenierojbetin19@gmail.com>
2025-01-19 23:11:05 -0500

    terminado2

--- src/components/Formularios/FormularioNC.jsx
+++ src/components/Formularios/FormularioNC.jsx
@@ -1,114 +1,144 @@
-
-import { Btn,BtnLimpia,ContentBtns,Title,Section,Form } from "../UI";
+import { Btn, BtnLimpia, ContentBtns, Title, Section, Form } from "../UI";
 import Inputs from "../Campos/Inputs";
 import TextTarea from "../Campos/Textarea";
 import ContentTable from "../Table/Table";
-import {v4 as uuidv4} from "uuid";
+import { v4 as uuidv4 } from "uuid";
 import { Api } from "../../api/ClienteService.js";
-import { useState,useEffect } from "react"
+import { useState, useEffect } from "react";
 import Loader from "../Loader/Loader";
+import styled from "styled-components";
 
+const ColorPickerContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  margin-bottom: 20px;
 
-const FormNuevaCategoria =()=>{
-    
-    // guardar los valores de los inputs y textos
-    const [titulo,setTitulo]=useState('');
-    const [descripcion,setDescripcion]=useState('');
-    const [colorPrimario,setColor]=useState('#000000');  // inicializamos el colore negro por default
-    const [codigoS,setCodigoS]=useState('');
+  label {
+    font-size: 16px;
+    color: #fff;
+  }
 
-    const [categorias,setCategorias]= useState([]);
+  input[type="color"] {
+    width: 50px;
+    height: 50px;
+    border: none;
+    cursor: pointer;
+    background: none;
+  }
+`;
 
-    const [loading,setLoading]= useState(false);
+const FormStyled = styled(Form)`
+  background-color: #000;
+  color: #fff;
+  padding: 20px;
+  border-radius: 8px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+`;
 
-    // carga la cosulta de todas la cetegorias para luego ser mostrado en la tabla
-    useEffect(()=>{
-        const ConsultaApi=async()=>{
-            const res= await Api.get("categorias");
-            if(res.status==200){ 
-                setCategorias(res.data)
-                setTimeout(()=>{
-                    setLoading(true)
-                },2500)
-            }
-        }
-        ConsultaApi();
-    },[])
+const FormNuevaCategoria = () => {
+  // guardar los valores de los inputs y textos
+  const [titulo, setTitulo] = useState("");
+  const [descripcion, setDescripcion] = useState("");
+  const [colorPrimario, setColor] = useState("#000000"); // inicializamos el color negro por default
+  const [codigoS, setCodigoS] = useState("");
 
-    /* capturamos los datos y lo enviamos para nuevo dato, se vuelve hacer la cosulta para
-|       actualizar las categorias*/
-    const EnviarDatos=async(e)=>{
-        e.preventDefault();
-        const data ={
-            id:uuidv4(),
-            titulo,
-            colorPrimario,
-            descripcion,
-            codigoS
-        }
-        const post = await Api.post('categorias',data);
-        if(post.status==201){
-            const res= await Api.get("categorias");
-            setCategorias(res.data) 
-        }
+  const [categorias, setCategorias] = useState([]);
+
+  const [loading, setLoading] = useState(false);
+
+  // carga la consulta de todas las categorias para luego ser mostrado en la tabla
+  useEffect(() => {
+    const ConsultaApi = async () => {
+      const res = await Api.get("categorias");
+      if (res.status === 200) {
+        setCategorias(res.data);
+        setTimeout(() => {
+          setLoading(true);
+        }, 2500);
+      }
+    };
+    ConsultaApi();
+  }, []);
+
+  /* capturamos los datos y lo enviamos para nuevo dato, se vuelve hacer la consulta para
+  actualizar las categorias*/
+  const EnviarDatos = async (e) => {
+    e.preventDefault();
+    const data = {
+      id: uuidv4(),
+      titulo,
+      colorPrimario,
+      descripcion,
+      codigoS,
+    };
+    const post = await Api.post("categorias", data);
+    if (post.status === 201) {
+      const res = await Api.get("categorias");
+      setCategorias(res.data);
     }
+  };
 
-    // eliminamos una categoria
-    const EliminarCategoria = async(id)=>{
-        await Api.delete(`categorias/${id}`);
-        const {data}= await Api.get("categorias");
-        setCategorias(data) 
-    }
+  // eliminamos una categoria
+  const EliminarCategoria = async (id) => {
+    await Api.delete(`categorias/${id}`);
+    const { data } = await Api.get("categorias");
+    setCategorias(data);
+  };
 
-    // Limpia todos los inputs
-    const resetText =()=>{
-        setTitulo('')
-        setDescripcion('')
-        setColor('#000000')
-        setCodigoS('')
-    }
+  // Limpia todos los inputs
+  const resetText = () => {
+    setTitulo("");
+    setDescripcion("");
+    setColor("#000000");
+    setCodigoS("");
+  };
 
+  return (
+    <Section>
+      <Title>Nueva Categoría</Title>
+      <FormStyled onSubmit={EnviarDatos}>
+        <Inputs
+          tipo="text"
+          titulo="Título"
+          valor={titulo}
+          ActualizarValor={setTitulo}
+        />
+        <TextTarea
+          titulo="Descripción"
+          ActualizarDescrip={setDescripcion}
+          valor={descripcion}
+        />
+        <ColorPickerContainer>
+          <label htmlFor="color">Color Primario</label>
+          <input
+            type="color"
+            id="color"
+            value={colorPrimario}
+            onChange={(e) => setColor(e.target.value)}
+          />
+        </ColorPickerContainer>
+        <Inputs
+          tipo="text"
+          titulo="Código de seguridad"
+          ActualizarValor={setCodigoS}
+          valor={codigoS}
+        />
 
-    return(
-        <Section>
-            <Title>Nueva Categoría</Title>
-            <Form onSubmit={EnviarDatos}>
-                <Inputs 
-                    tipo="text" 
-                    titulo="titulo"
-                    valor={titulo}
-                    ActualizarValor={setTitulo}
-                />
-                <TextTarea 
-                    titulo="Descripcion"
-                    ActualizarDescrip={setDescripcion}
-                    valor={descripcion}
-                />
-                <Inputs 
-                    tipo="color" 
-                    titulo="color"
-                    ActualizarValor={setColor}
-                    valor={colorPrimario}
+        <ContentBtns>
+          <div>
+            <Btn>Guardar</Btn>
+            <BtnLimpia type="reset" value="Limpiar" onClick={resetText} />
+          </div>
+        </ContentBtns>
+      </FormStyled>
+      {loading ? (
+        <ContentTable Datos={categorias} EliminaDato={EliminarCategoria} tipo={true} />
+      ) : (
+        <Loader />
+      )}
+    </Section>
+  );
+};
 
-                />
-                <Inputs 
-                    tipo="text" 
-                    titulo="Codigo de seguridad"
-                    ActualizarValor={setCodigoS}
-                    valor={codigoS}
-                />
-
-                <ContentBtns>
-                    <div>
-                        <Btn>Guardar</Btn>
-                        <BtnLimpia type="reset" value="Limpiar" onClick={resetText} /> 
-                    </div>
-                </ContentBtns>
-            </Form>
-            {loading? <ContentTable Datos={categorias} EliminaDato={EliminarCategoria}  tipo={true} />: <Loader />}
-        </Section>
-    )
-}
-
-export default FormNuevaCategoria
-
+export default FormNuevaCategoria;
--- src/components/Formularios/FormularioNV.jsx
+++ src/components/Formularios/FormularioNV.jsx
@@ -1,144 +1,180 @@
-import { Btn,BtnLimpia,ContentBtns,Title,Section,Form } from "../UI";
-import Inputs from "../Campos/Inputs";
-import TextTarea from "../Campos/Textarea";
-import { Link } from "react-router-dom";
-import ListaOpciones from "../ListaOpciones/ListaOpciones";
-import { useState,useEffect } from "react";
-import { Api } from "../../api/ClienteService.js";
-import {v4 as uuidv4} from "uuid"
-import styled from "styled-components";
-import ContentTable from "../Table/Table";
-import Loader from "../Loader/Loader";
-
-const DivAcciones =styled.div`
-    @media screen and (max-width: 768px){
-        width: 100%;
-        display: flex;
-        justify-content: space-between;
-    }
-`
-
-
-
-
-
-
-const FormNuevoVideo =(props)=>{
-    const [titulo,setTitulo]=useState('');
-    const [urlVideo,setUrlVideo]=useState('');
-    const [urlImg,setUrlImg]=useState('https://i.ytimg.com/vi//maxresdefault.jpg');
-    const [categoria,setCategoria]=useState('');
-    const [descripcion,setDescripcion]=useState('');
-    const [codigoS,setCodigoS]=useState('');
+import {
+    Btn,
+    BtnLimpia,
+    ContentBtns,
+    Title,
+    Section,
+    Form,
+  } from "../UI";
+  import Inputs from "../Campos/Inputs";
+  import TextTarea from "../Campos/Textarea";
+  import { Link } from "react-router-dom";
+  import ListaOpciones from "../ListaOpciones/ListaOpciones";
+  import { useState, useEffect } from "react";
+  import { Api } from "../../api/ClienteService.js";
+  import { v4 as uuidv4 } from "uuid";
+  import styled from "styled-components";
+  import ContentTable from "../Table/Table";
+  import Loader from "../Loader/Loader";
+  
+  const DivAcciones = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    gap: 10px;
     
-    const [videos,setVideos]= useState([])
-
-    const [loading,setLoading]= useState(false)
-
-    // carga la cosulta de todas la cetegorias para luego ser mostrado en la tabla
-    useEffect(()=>{
-        const ConsultaApi=async()=>{
-            const res= await Api.get("videos");
-            if(res.status==200){
-                setVideos(res.data);
-                setTimeout(()=>{
-                    setLoading(true)
-                },2500)
-            }
+    @media screen and (max-width: 768px) {
+      flex-direction: column;
+      align-items: stretch;
+    }
+  `;
+  
+  const StyledForm = styled(Form)`
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    padding: 20px;
+    background-color: #000;
+    color: #fff;
+    border-radius: 10px;
+    box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+  `;
+  
+  const StyledContentBtns = styled(ContentBtns)`
+    margin-top: 20px;
+    display: flex;
+    justify-content: space-between;
+    gap: 20px;
+  
+    @media screen and (max-width: 768px) {
+      flex-direction: column;
+      align-items: stretch;
+    }
+  `;
+  
+  const StyledLink = styled(Link)`
+    text-decoration: none;
+  `;
+  
+  const FormNuevoVideo = (props) => {
+    const [titulo, setTitulo] = useState("");
+    const [urlVideo, setUrlVideo] = useState("");
+    const [urlImg, setUrlImg] = useState("https://i.ytimg.com/vi//maxresdefault.jpg");
+    const [categoria, setCategoria] = useState("");
+    const [descripcion, setDescripcion] = useState("");
+    const [codigoS, setCodigoS] = useState("");
+  
+    const [videos, setVideos] = useState([]);
+    const [loading, setLoading] = useState(false);
+  
+    useEffect(() => {
+      const fetchVideos = async () => {
+        const res = await Api.get("videos");
+        if (res.status === 200) {
+          setVideos(res.data);
+          setTimeout(() => {
+            setLoading(true);
+          }, 2500);
         }
-        ConsultaApi();
-    },[])
-
-
-    const EnviarDatos=async(e)=>{
-        e.preventDefault()
-        const datos ={
-            id:uuidv4(),
-            titulo,
-            urlVideo,
-            urlImg,
-            categoria,
-            descripcion,
-            codigoS
-        }
-        const post = await Api.post("videos",datos);
-        if(post.status==201){
-            setLoading(false)
-            const res= await Api.get("videos");
-            setVideos(res.data)
-            setTimeout(()=>{
-                setLoading(true)
-            },2500)
-        }
-    }
-
-    const EliminarVideo = async(id)=>{
-        await Api.delete(`videos/${id}`);
-        const {data}= await Api.get("videos");
-        setVideos(data) 
-    }
-
-    const resetText =()=>{
-        setTitulo('')
-        setDescripcion('')
-        setUrlVideo('')
-        setCodigoS('')
-    }
-    const url =(urlV)=>{
-        const img = urlV.slice(-11)
-        setUrlVideo(urlV)
-        setUrlImg(`https://i.ytimg.com/vi/${img}/maxresdefault.jpg`);
-        return null
-    }
-    return(
-        <Section>
-            <Title>Nuevo Video</Title>
-            <Form onSubmit={EnviarDatos}>
-                <Inputs 
-                    tipo="text" 
-                    titulo="Titulo"
-                    valor={titulo}
-                    ActualizarValor={setTitulo}
-                />
-                <Inputs
-                    tipo="text" 
-                    titulo="Link del video"
-                    valor={urlVideo}
-                    ActualizarValor={url}
-                />
-                <Inputs 
-                    tipo="text" 
-                    titulo="Link del imagen del video"
-                    valor={urlImg}
-                />
-                <ListaOpciones 
-                    categorias={props.categorias} 
-                    valor={categoria}
-                    ActualizarCategoria={setCategoria}
-                />
-                <TextTarea 
-                    titulo="Descripcion"  
-                    ActualizarDescrip={setDescripcion}
-                />
-                <Inputs 
-                    tipo="text" 
-                    titulo="Codigo de seguridad"
-                    valor={codigoS}
-                    ActualizarValor={setCodigoS}
-                />
-
-                <ContentBtns>
-                    <DivAcciones>
-                        <Btn>Guardar</Btn>
-                        <BtnLimpia type="reset" value="Limpiar" onClick={resetText} /> 
-                    </DivAcciones>
-                    <Link to="/NuevaCategoria" ><Btn>Nueva Categoria</Btn></Link>
-                </ContentBtns>
-            </Form>
-            {loading?  <ContentTable Datos={videos} EliminaDato={EliminarVideo} /> :<Loader />}
-        </Section>
-    )
-}
-
-
-export default FormNuevoVideo
+      };
+      fetchVideos();
+    }, []);
+  
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      const datos = {
+        id: uuidv4(),
+        titulo,
+        urlVideo,
+        urlImg,
+        categoria,
+        descripcion,
+        codigoS,
+      };
+      const post = await Api.post("videos", datos);
+      if (post.status === 201) {
+        setLoading(false);
+        const res = await Api.get("videos");
+        setVideos(res.data);
+        setTimeout(() => {
+          setLoading(true);
+        }, 2500);
+      }
+    };
+  
+    const handleDelete = async (id) => {
+      await Api.delete(`videos/${id}`);
+      const { data } = await Api.get("videos");
+      setVideos(data);
+    };
+  
+    const resetForm = () => {
+      setTitulo("");
+      setDescripcion("");
+      setUrlVideo("");
+      setCodigoS("");
+    };
+  
+    const updateImageUrl = (urlV) => {
+      const imgId = urlV.slice(-11);
+      setUrlVideo(urlV);
+      setUrlImg(`https://i.ytimg.com/vi/${imgId}/maxresdefault.jpg`);
+    };
+  
+    return (
+      <Section>
+        <Title>Nuevo Video</Title>
+        <StyledForm onSubmit={handleSubmit}>
+          <Inputs
+            tipo="text"
+            titulo="Título"
+            valor={titulo}
+            ActualizarValor={setTitulo}
+          />
+          <Inputs
+            tipo="text"
+            titulo="Link del video"
+            valor={urlVideo}
+            ActualizarValor={updateImageUrl}
+          />
+          <Inputs
+            tipo="text"
+            titulo="Link de la imagen del video"
+            valor={urlImg}
+            disabled
+          />
+          <ListaOpciones
+            categorias={props.categorias}
+            valor={categoria}
+            ActualizarCategoria={setCategoria}
+          />
+          <TextTarea
+            titulo="Descripción"
+            ActualizarDescrip={setDescripcion}
+          />
+          <Inputs
+            tipo="text"
+            titulo="Código de seguridad"
+            valor={codigoS}
+            ActualizarValor={setCodigoS}
+          />
+          <StyledContentBtns>
+            <DivAcciones>
+              <Btn>Guardar</Btn>
+              <BtnLimpia type="reset" value="Limpiar" onClick={resetForm} />
+            </DivAcciones>
+            <StyledLink to="/NuevaCategoria">
+              <Btn>Nueva Categoría</Btn>
+            </StyledLink>
+          </StyledContentBtns>
+        </StyledForm>
+        {loading ? (
+          <ContentTable Datos={videos} EliminaDato={handleDelete} />
+        ) : (
+          <Loader />
+        )}
+      </Section>
+    );
+  };
+  
+  export default FormNuevoVideo;
