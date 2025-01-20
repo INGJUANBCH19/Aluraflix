e0aecde235343fdf2ba9fbfb4a2736ae52464d19
Juan Camilo Betin <ingenierojbetin19@gmail.com>
2025-01-19 22:38:55 -0500

    TERMINADO1

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
-import Logo from "../../assets/img/LogoMain.png"
-import styled from "styled-components"
-import { P} from "../UI"
+import Logo from "../../assets/img/LogoMain.png";
+import styled from "styled-components";
+import { P } from "../UI";
 
 const Footer = styled.footer`
     width: 100%;
@@ -12,43 +12,61 @@ const Footer = styled.footer`
     align-items: center;
     padding: 20px 0 30px 0;
     border-top: 3px solid rgba(42, 122, 228, 1);
-`
+`;
 const Div = styled.div`
-    
     margin-top: 20px;
     display: flex;
     align-items: center;
     gap: 5px;
-`
+`;
 const A = styled.a`
     text-decoration: none;
     color: white;
     font-size: 30px;
     padding: 5px;
-    i{
-        transition: all .4s ease-in-out;
+    i {
+        transition: all 0.4s ease-in-out;
     }
-    i:hover{
-        /* transform: scale(1.1px); */
+    i:hover {
         transform: scale(1.2);
-        transition: all .4s ease-in-out;
+        transition: all 0.4s ease-in-out;
     }
-` 
-const Stronge =styled.span`
+`;
+const Stronge = styled.span`
     font-weight: 800;
     color: rgba(42, 122, 228, 1);
-`
+`;
 
-export default ()=>{
-    return(
+export default () => {
+    return (
         <Footer>
-            <img  src={Logo} />
+            <img src={Logo} alt="Logo" />
             <Div>
-                <P>&copy; 2025 |<Stronge>AluraLatam</Stronge></P>
-                <A href="" target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-github"></i></A>
-                <A href="" target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-linkedin"></i></A>
+                <P>
+                    &copy; 2025 |<Stronge>AluraLatam</Stronge>
+                </P>
+                <A
+                    href="https://github.com/INGJUANBCH19"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <i className="fa-brands fa-github"></i>
+                </A>
+                <A
+                    href="https://www.linkedin.com/in/juan-betin-5092172b6/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <i className="fa-brands fa-linkedin"></i>
+                </A>
+                <A
+                    href="https://www.instagram.com/jbetinchamat/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <i className="fa-brands fa-instagram"></i>
+                </A>
             </Div>
-           
         </Footer>
-    )
-}
+    );
+};
